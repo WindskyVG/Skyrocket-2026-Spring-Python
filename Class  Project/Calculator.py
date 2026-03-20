47b104dfd9a16456e66972b3ab2170ef2cc69106
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-03-19 18:12:57 -0700

    added other_random_stuff, need to debug and ask teacher. also deleted answer = None. no use.

--- Class  Project/Calculator.py
+++ Class  Project/Calculator.py
@@ -1,12 +1,12 @@
 #Calculator
 import time
-answer = None
 history = []
-print("The math operations are +, -, * (times), / (divide/fractions), and %. \n type q to quit and history to show history.")
+print("The math operations are +, -, * (times), / (divide/fractions), and %. \n type q to quit and history to show history. All other things do not work.")
 
 while True:
     User_input = input("Enter your math equation (or something else): ") #6/3
     Math_operations = ['+', '-', '*', '/', '%']
+    other_random_stuff = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','!','@','#','$','','^','&','(',')',',','<','.','>','?','"',"'",';',':','=','[',']','{','}','|','`','~']
 
     op = None
     for o in Math_operations:
@@ -21,8 +21,11 @@ while True:
         time.sleep(2)
         print("Bye bye")
         break
-
-    elif User_input != 'history':#the '!' is equal to not
+    elif User_input.lower() == 'history' and User_input == other_random_stuff:
+        for a in answer:
+            print("Sorry that is not available yet or a valid answer.")
+            print("--------------------------------------------------")
+    elif User_input.lower() != 'history' and User_input != other_random_stuff:#the '!' is equal to not
         num1 = float(parts[0].strip())
         num2 = float(parts[1].strip())
 
@@ -42,20 +45,16 @@ while True:
         print("The answer is: ")
         answer = num1 - num2
 
-    if answer == int(answer) and answer != None:
+    if answer == int(answer):
         print(int(answer))
-    elif answer == float(answer) and answer != None:
+    elif answer == float(answer):
         print(answer)
 
     history.append(User_input + ' = ' + str(answer))
-    if User_input == 'history':
+    if User_input.lower() == 'history':
         print("-----------History------------")
         print("Please ignore the last statement of your history.")
         for item in history:
             print(item)
         print("------------------------------")
 
-    if answer == None and User_input == 'history':
-        for a in answer:
-            print("Sorry that is not available yet or a valid answer.")
-            print("--------------------------------------------------")
