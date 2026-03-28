cfad46440cc93f702e6bcc3290417b24312d1d51
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-03-28 13:17:08 -0700

    sjkhd

--- Class  Project/Rock, Paper, Scissors.py
+++ Class  Project/Rock, Paper, Scissors.py
@@ -7,33 +7,29 @@ Wins = 0
 Loses = 0
 Draws = 0
 Coins = 0
-print("type q or quit to quit.\ntype outcomes (or o) to see how many wins you have.\n(You can also type s for scissors, r for rock. or p for paper. \nYou can also type Tool Shop (or TS) to see the shop.")
 while True:
-    Computer = random.randint(1, 3)
-    #rock = 1, paper = 2, scissors = 3
-    User_input = input("type rock, paper, or scissors: ")
-    if User_input.lower() == "outcomes" or User_input.lower() == "outcome" or User_input.lower() == "o":
-        print("Here are your results: ")
-        print("----------------------------")
-        print("Wins = " + str(Wins))
-        print("Loses = " + str(Loses))
-        print("Draws = " + str(Draws))
-        print("----------------------------")
-    elif User_input.lower() == "tools shop" or User_input.lower() == "ts":
-        Gun_num = 1
+    Input = input("type q or quit to quit.\ntype outcomes (or o) to see how many wins you have.\n(You can also type s for scissors, r for rock. or p for paper. \nYou can also type Tool Shop (or TS) to see the shop.\nDon't type rock or paper or scissors yet and don't close this tab, you will lose all of your progress.\n Enter start to start. (Or other): ")
+    if Input.lower() == "tools shop" or Input.lower() == "ts":
+        Gun = 1
         print("Your coins: " + str(Coins))
         print("---------------------------")
         print("Item: Gun\nCost: 1000")
         print("---------------------")
         User_shop_input = input("type the name of the item to buy: ")
         if User_shop_input.lower() == "gun":
-            while True:
-                User_check = input("are you sure? Type y for yes and no for no: ")
-                if User_check.lower() == "y":
-                    print("ok.")
-                    items = ['Gun']
-                    break
-    elif User_input.lower() == "q" or User_input.lower() == "quit":
+            User_check = input("are you sure? Type y for yes and no for no: ")
+            if User_check.lower() == "y" or User_check.lower() == "yes":
+                print("ok.")
+                if Coins >= 0:
+                    items.append(User_shop_input)
+                    print("exiting tools shop")
+                else:
+                    print("error. Purchase unsuccessful.")
+                    print("exiting tool shop")
+            elif User_check.lower() == "no" or User_check.lower() == "n":
+                print("ok")
+                print("exiting tools shop")
+    elif Input.lower() == "q" or Input.lower() == "quit":
         print("loading.")
         time.sleep(1)
         print("loading..")
@@ -42,61 +38,72 @@ while True:
         time.sleep(1)
         print("Bye bye!")
         time.sleep(1)
-        break
-    elif User_input.lower() == "rock" or User_input.lower() == "r":
-        if Computer == 1:
-            print("-------------------------------")
-            print("Computer choose rock")
-            print("draw.")
-            print("--------------------")
-            Draws += 1
-        elif Computer == 2:
-            print("----------------------------")
-            print("Computer choose paper")
-            print("You lose.")
-            print("---------------------------")
-            Loses += 1
-        else:
-            print("-------------------------------------")
-            print("Computer choose scissors")
-            print("You win!")
-            Wins += 1
-    elif User_input.lower() == "paper" or User_input.lower() == "p":
-        if Computer == 1:
-            print("--------------------")
-            print("Computer choose rock")
-            print("You win!")
-            print("---------------------")
-            Wins += 1
-        elif Computer == 2:
-            print("--------------------")
-            print("Computer choose paper")
-            print("draw.")
-            print("--------------------")
-            Draws += 1
-        else:
-            print("--------------------")
-            print("Computer choose scissors")
-            print("You lose.")
-            print("---------------------")
-            Loses += 1
-    elif User_input.lower() == "scissors" or User_input.lower() == "s":
-        if Computer == 1:
-            print("--------------------")
-            print("Computer choose rock")
-            print("You lose!")
-            print("---------------------")
-            Loses += 1
-        elif Computer == 2:
-            print("--------------------")
-            print("Computer choose paper")
-            print("You win!")
-            Wins += 1
-        else:
-            print("--------------------")
-            print("Computer choose scissors")
-            print("draw.")
-            print("---------------------")
-            Draws += 1
     else:
-        print("Sorry that answer is unavailable...")
+        while True:
+            Computer = random.randint(1, 3)
+            # rock = 1, paper = 2, scissors = 3
+            User_input = input("type rock, paper, or scissors: ")
+            if User_input.lower() == "outcomes" or User_input.lower() == "outcome" or User_input.lower() == "o":
+                print("Here are your results: ")
+                print("----------------------------")
+                print("Wins = " + str(Wins))
+                print("Loses = " + str(Loses))
+                print("Draws = " + str(Draws))
+                print("----------------------------")
+            elif User_input.lower() == "rock" or User_input.lower() == "r":
+                if Computer == 1:
+                    print("-------------------------------")
+                    print("Computer choose rock")
+                    print("draw.")
+                    print("--------------------")
+                    Draws += 1
+                elif Computer == 2:
+                    print("----------------------------")
+                    print("Computer choose paper")
+                    print("You lose.")
+                    print("---------------------------")
+                    Loses += 1
+                else:
+                    print("-------------------------------------")
+                    print("Computer choose scissors")
+                    print("You win!")
+                    Wins += 1
+            elif User_input.lower() == "paper" or User_input.lower() == "p":
+                if Computer == 1:
+                    print("--------------------")
+                    print("Computer choose rock")
+                    print("You win!")
+                    print("---------------------")
+                    Wins += 1
+                elif Computer == 2:
+                    print("--------------------")
+                    print("Computer choose paper")
+                    print("draw.")
+                    print("--------------------")
+                    Draws += 1
+                else:
+                    print("--------------------")
+                    print("Computer choose scissors")
+                    print("You lose.")
+                    print("---------------------")
+                    Loses += 1
+            elif User_input.lower() == "scissors" or User_input.lower() == "s":
+                if Computer == 1:
+                    print("--------------------")
+                    print("Computer choose rock")
+                    print("You lose!")
+                    print("---------------------")
+                    Loses += 1
+                elif Computer == 2:
+                    print("--------------------")
+                    print("Computer choose paper")
+                    print("You win!")
+                    Wins += 1
+                else:
+                    print("--------------------")
+                    print("Computer choose scissors")
+                    print("draw.")
+                    print("---------------------")
+                    Draws += 1
+            else:
+                print("Sorry that answer is unavailable...")
--- Homework/range homework.py
+++ Homework/range homework.py
@@ -40,7 +40,7 @@ print("---------------3-----------------")
 
 for i in range(5):
     for j in range(i*2):#if this is also a 10, then it will be a 10x10 thing,but since it is i, it is upside-down
-        print('', end ='') #<--this mkes a new line
+        print('', end ='') #<--this makes a new line
     for j in range(5-i):#the i changes it
         print('*', end ='')
     for j in range(0-i):
@@ -57,7 +57,7 @@ print("---------------4-----------------")
 
 for i in range(5):
     for j in range(4-i):#if this is also a 10, then it will be a 10x10 thing,but since it is 10-i, it is upside-down
-        print(' ', end ='') #<--this mkes a new line
+        print(' ', end ='') #<--this makes a new line
     for j in range(i):#the i changes it
         print('*', end ='')
     for j in range(i):
@@ -79,7 +79,7 @@ print("---------------5-----------------")
 
 for i in range(3):
     for j in range(3-i):#if this is also a 10, then it will be a 10x10 thing,but since it is 10-i, it is upside-down
-        print(' ', end ='') #<--this mkes a new line
+        print(' ', end ='') #<--this makes a new line
     for j in range(i):#the i changes it
         print('*', end ='')
     for j in range(i):
@@ -87,7 +87,7 @@ for i in range(3):
     print("\n")
 for i in range(3):
     for j in range(i):#if this is also a 10, then it will be a 10x10 thing,but since it is i, it is upside-down
-        print(' ', end ='') #<--this mkes a new line
+        print(' ', end ='') #<--this makes a new line
     for j in range(3-i):#the i changes it
         print('*', end ='')
     for j in range(3-i):
