05acff80d3f21e636509e4af077187727a6a4c1d
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-04-04 14:58:33 -0700

    Update Factor.py

--- Class  Project/Factor.py
+++ Class  Project/Factor.py
@@ -1,16 +1,8 @@
 Factors = []
-j = 0
 while True:
-    Number = input("Enter a number to factor: ")
-    if Number == int(Number):
-        Number = int(Number)
-        for i in range(1, 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
-            x = 0
-        if x <= Number:
-            j = x
-        Factor = (Number/j)
-        if Factor == int(Factor):
-            Factors.append("Factors: " + Factor)
-            
-            4%2=0
-            
+    Number = int(input("Enter a number to factor: "))
+    for i in range(1, (Number + 1)):
+        if Number % i == 0:
+            Factors.append(i)
+    print("Here are the Factors: ")
+    print(Factors)
