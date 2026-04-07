e615c5c1611e7a93c9d8d63bffd1e8105f66a306
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-04-06 21:46:56 -0700

    Update Quiz game.py

--- Home Games/Quiz game.py
+++ Home Games/Quiz game.py
@@ -1,5 +1,6 @@
+#need to finish Minecraft and Minecraft foxes theme...
 import time
-print("Welcome to the Quiz Game!\nYou can type quit to quit\nYou can also type 'results' to show the results\n(Made by me, the amazing Sophia!!!)")
+print("Welcome to the Quiz Game!\nYou can also type 'results' to show the results\n(Made by me, the amazing Sophia!!!)")
 print("Here are the rules:\nThere can be multiple correct choices\nChoose from A, B, C, D, or E!(Or sometimes, F!)\nHave fun!")
 User_name = input("What is your name? \nPlease enter your name: ")
 print("Welcome " + User_name + "!")
@@ -9,7 +10,7 @@ time.sleep(1)
 while True:
     User_chose_theme = input("Choose a theme from the following list: \n-----------------\nFoxes\nMinecraft\nMinecraft Foxes\n-----------------\nChoose a theme from the list: ")
     if User_chose_theme.lower() == "foxes" or User_chose_theme.lower() == "fox":
-        q1 = input("First question:\nWhat are the tails of a fox for?\nA. To keep balance\nB. To keep warm\nC. Only the one above is correct\nD. To Communicate\nE. To attract mates\n---------------\nEnter the your choice: ")
+        q1 = input("First question:\nWhat is the tail of a fox for?\nA. To keep balance\nB. To keep warm\nC. Only the one above is correct\nD. To Communicate\nE. To attract mates\n---------------\nEnter the your choice: ")
         if q1.lower() == "a":
             print("One of the correct choices!")
             player_correct_answers += 1
@@ -48,7 +49,11 @@ while True:
             print("Wrong!")
             time.sleep(1)
             print("But since it wasn't in the choices, I won't count that!")
-        print("That's it for today! Here are your results: \n--------------------\nYou got " + player_correct_answers + " correct!\nYou got" + player_incorrect_answers + " incorrect!\n----------------------\nLet's play again!")
+        print("That's it for today! Here are your results from all total games: \n--------------------\nYou got " + str(player_correct_answers) + " correct!\nYou got " + str(player_incorrect_answers) + " incorrect!\n----------------------\nLet's play again!")
         time.sleep(1)
     elif User_chose_theme.lower() == "minecraft" or User_chose_theme.lower() == "minecraft fox" or User_chose_theme.lower() == "minecraft foxes":
         print("Sorry that is not ready yet...\nChoose another theme!")
+    elif User_chose_theme.lower() == "results" or User_chose_theme.lower() == "result":
+        print("Here are your results from all total games: \n--------------------\nYou got " + str(player_correct_answers) + " correct!\nYou got " + str(player_incorrect_answers) + " incorrect!\n----------------------\nLet's play again!")
+    else:
+        print("Sorry, something went wrong")
