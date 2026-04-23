f990232c6e957ffffdc665848ba7ad5cd4168d11
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-04-22 19:23:03 -0700

    make practice questions

---
+++ Homework/len, hex, and bin code experianments.py
@@ -0,0 +1,12 @@
+hi = "ljasdklhfaajkfhcsdcsd"
+len(hi)
+print(len(hi))
+print("Conclusion: len just tells you the number of stuff in lists or strings or ints")
+lalala = 9
+bin(lalala)
+print(bin(lalala))
+print("Conclusion: bin just make it into its 'binary' form, whatever that means...")
+world = 16
+hex(world)
+print(hex(world))
+print("Conclusion: hex just turns numbers into, like, a code, for numbers, aka its 'hexadecimal' form")
