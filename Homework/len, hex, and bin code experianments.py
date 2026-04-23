74f5f7053bdd1ea83b20a05cefd50594b3b6af34
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-04-22 19:52:22 -0700

    Update len, hex, and bin code experianments.py

--- Homework/len, hex, and bin code experianments.py
+++ Homework/len, hex, and bin code experianments.py
@@ -2,11 +2,11 @@ hi = "ljasdklhfaajkfhcsdcsd"
 len(hi)
 print(len(hi))
 print("Conclusion: len just tells you the number of stuff in lists or strings or ints")
-lalala = 9
+lalala = 12
 bin(lalala)
 print(bin(lalala))
 print("Conclusion: bin just make it into its 'binary' form, whatever that means...")
-world = 16
+world = 18
 hex(world)
 print(hex(world))
 print("Conclusion: hex just turns numbers into, like, a code, for numbers, aka its 'hexadecimal' form")
