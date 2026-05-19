bde6763e47f633e098b425c252966bbbba713094
Diamondgemsfox <diamondgemsfox@gmail.com>
2026-05-19 15:52:21 -0700

    Update Final Project.py

--- Class  Project/Final Project.py
+++ Class  Project/Final Project.py
@@ -7,30 +7,8 @@ import time
 # Main Game Loop
 # ==================================================
 
-player_name = input("-----------------------\n'Hi! Welcome to the fox adoption center!' said ???\n'My name is Vulpix, the caretaker of these foxes.' said Vulpix\n'What is your name?' Vulpix asks\nPlease enter your name: ")
-print("'Welcome " + player_name + "!'Vulpix says")
-time.sleep(2)
-print("'You can adopt a fox for $50!'")
-time.sleep(2)
-print("'What is that? You don't have enough money?'")
-time.sleep(1)
-print("'Well, since it's your first time here you can adopt a fox for free!'")
-time.sleep(1)
-print("'And we will pay you to take care of this baby fox!'exclaimed Vulpix")
-time.sleep(2)
-print("You got a baby fox!")
-time.sleep(2)
 fox_name = input("'What are you going to name your fox?'asked Vulpix\nPlease enter your fox's name: ")
-print("'" + fox_name + " is a great name!'said Vulpix")
-time.sleep(2)
-print("'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'yowled ???")
-time.sleep(1)
-print("'Oh my, it seems Jasper has awoken!'said Vulpix")
-time.sleep(1)
-print("'eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'yowled Jasper")
-time.sleep(1)
-print("'Well, I'd better go! Bye!'said Vulpix")
-time.sleep(2)
+
 age_max = 361
 age = 1
 happy_bar = 100
@@ -38,6 +16,7 @@ hunger_bar = 100
 attack = 10
 player_money = 1000
 player_sleep_bar = 100
+
 def stats():
     print("""==================================
           Your Fox's Stats:
@@ -61,6 +40,17 @@ for day_num in range(age_max):
     print(" DAY", day_num)
     print("======================================")
 
+    if day_num == 360:
+        print("Your pet fox have died.")
+        time.sleep(2)
+        print("But she had a great time, and died happily ever after.")
+        time.sleep(2)
+        print("You prepared a funeral, and your friends and family attended too, and also Vulpix.")
+        time.sleep(2)
+        print("Nobody said a word, but as you looked up, you saw a shape dancing in the sky, and you knew it was " + fox_name + ".")
+        time.sleep(2)
+        print("You got the peaceful ending!")
+
     def workday():
         print("You have to go to work today, and didn't have a whole day to take care of you fox.")
         time.sleep(1.5)
@@ -68,10 +58,12 @@ for day_num in range(age_max):
         print("You just woke up and saw " + fox_name + "staring at you.\nYou just remembered that you got a pet fox!")
         time.sleep(1)
         workday()
-        actions = input("""What will you do first?
-        1. Feed """ + str(fox_name) + """
-        2. Walk""")
+        #actions = input("""What will you do first?
+        #1. Feed """ + str(fox_name) + """
+        #2. Walk""")
     elif day_num % 6 == 0:
         print("")
     elif day_num%7 == 0:
         print("")
+    elif day_num == 360:
+        print("yay")
